0581d5cd4da42e3ab1266e05c136169106508cee
Djkde <sergio.estrella@utp.edu.co>
2020-11-09 20:55:18 -0500

    Landing page created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
       <Header />
-        <h1>Welcome to the Torre Match Application!</h1>
-        <h3>See if your profile fits with these dream job</h3>
         <Footer/>
       </div>
-      <Route path="/jobs" component={JobsScreen}/>
-      <Route path="/profiles" component={ProfilesScreen}/>
-      <Route path="/match" component={MatchScreen}/>
+      
       <Route path="/" component={HomeScreen} exact/>
+      
     </BrowserRouter>
 
   );
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function HomeScreen(){
+    return(
+        <main className="HomeScreen">
+        <div>
+            <h1>Welcome to the Torre Match Application!</h1>
+            <h3>See if your profile fits with these dream job</h3>
+        </div>
+            <a href="/profiles" className="home-button">Search profiles</a>
+            <div>
+            <p>If you don't have a profile</p>
+            <a href="https://torre.co">Create one here!</a>
+
+            </div>
+        </main>
+    )
+}
